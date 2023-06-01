839fac58e938430ae814f887d5fd4f6905b16c03
aamir_shayan <aamir.shayan@qordata.com>
2023-06-01 15:23:37 +0500

    Master project

---
+++ src/test/java/com/javakar/springdemomaster/SpringDemoMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javakar.springdemomaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
