189e0d18bbc558710dafa7f06183952a90e937af
aamir_shayan <aamir.shayan@qordata.com>
2023-06-19 15:53:04 +0500

    Main method application template

--- src/main/java/com/javakar/springcreiteriapaginingfilteringsorting/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javakar.springcreiteriapaginingfilteringsorting.repository;
-
-import com.javakar.springcreiteriapaginingfilteringsorting.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
