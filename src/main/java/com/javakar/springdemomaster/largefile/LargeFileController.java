b7548678cc782524b3c42672101cb707382ea148
aamir_shayan <aamir.shayan@qordata.com>
2023-09-21 16:31:24 +0500

    fix time put issue

--- src/main/java/com/javakar/springdemomaster/largefile/LargeFileController.java
+++ src/main/java/com/javakar/springdemomaster/largefile/LargeFileController.java
@@ -23,8 +23,8 @@ public class LargeFileController {
     @GetMapping("/extract-and-download-csv")
     public ResponseEntity<StreamingResponseBody> extractAndDownloadCsvFromZip(HttpServletResponse response) throws IOException {
         String myUrl = "place your actual url here, remember I am extracting a zip folder and then downloading a 10gb large csv file";
-        URL url = new URL(myUrl);
         String targetCsvFileName = "name-of-your-csv-file.csv";
+        URL url = new URL(myUrl);
 
         response.setContentType("text/csv");
         response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + targetCsvFileName + "\"");
